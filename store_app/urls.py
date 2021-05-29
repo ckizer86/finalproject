9f472d5a8d4f72dc1190164d26ad3bfe3309cbb5
Crystal Kizer <ckizer86@yahoo.com>
2021-05-28 20:10:39 -0500

    created login, registration, and category templates with views and url paths

--- store_app/urls.py
+++ store_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('login', views.login_page),
+    path('loginuser', views.login),
+    path('register', views.register_page),
+    path('registeruser', views.register),
+    path('cat/<int:id>', views.category),
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     
     return render(request, "index.html")
+
+def login_page(request):
+
+    return render(request, "login.html")
+
+def login(request):
+    
+    return redirect('/login')
+
+def register_page(request):
+
+    return render(request, "register.html")
+
+def register(request):
+
+    return redirect('/register')
+
+def category(request, id):
+
+    return render(request, "category.html")
